c0c2f65ea6bb3498bcc52c1d6af8595c75ec23c4
claurennt <c.benedetti1989@gmail.com>
2022-07-26 12:24:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [post, setPost] = useState();
+  const [id, setId] = useState(1);
+
+  //use effect only runs after the component did mount
+  //with empty [] the use effect will run only once
+  //if we put a variable inside the [] the useEffect will run every time the value of that variable changes
+  useEffect(() => {
+    // fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    //   .then((res) => {
+
+    //     return res.json();
+    //   })
+    //   .then((data) => setPost(data))
+    //   .catch((err) => console.log(err));
+
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => setPost(res.data))
+      .catch((err) => console.log("error", err));
+  }, [id]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>API FETCH</h1>
+      <button onClick={() => setId((prev) => prev + 1)}>
+        fetch again with new id
+      </button>
+      <p>{post?.title}</p>
+
+      {/* {posts && posts.map((post) => <p>{post.title}</p>)} */}
+      {/* {posts?.map((post) => (
+        <p>{post.title}</p>
+      ))} */}
     </div>
   );
 }
